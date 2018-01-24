5112255acfca03586f9f2f13e3cd886cb755e904
Eliot Reyes <eliotmij@gmail.com>
2018-01-24 14:36:23 -0800

    cors

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require("express");
 var fileUploader = require('express-fileupload')
 var app = express();
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
+
 app.use(fileUploader());
 var bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded 
