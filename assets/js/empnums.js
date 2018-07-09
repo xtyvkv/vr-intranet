04bd604d3c0964a9221a3a90dd62adc6be8f4115
Eliot Reyes <EReyes@vitalresearch.local>
2018-07-09 15:16:07 -0700

    feature complete: employee numbers page

---
+++ assets/js/empnums.js
@@ -0,0 +1,36 @@
+$(document).ready( function () {
+    // $('#contact-info-table').DataTable();
+    init();
+} );
+
+function init(){
+    fetch('http://localhost:8180/api/empnums')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(el =>{
+            processRecord(el);
+        });
+    $('#contact-info-table').DataTable();
+
+    });
+}
+
+function processRecord(data){
+    let tbody = $('#contact-info-tbody');
+    
+    let row = $('<tr>');
+
+    let s = `<td>${testNull(data['FirstName'])} ${testNull(data['LastName'])}</td>
+            <td>${testNull(data['CellPhone'])}</td>
+            <td>${testNull(data['HomePhone'])}</td>
+            <td>${testNull(data['WorkNumber'])}</td>
+            <td>${testNull(data['CallInstructions'])}</td>
+    `;
+
+    row.append(s);
+    tbody.append(row);
+}
+
+function testNull(s){
+    return s ? s: "";
+}
